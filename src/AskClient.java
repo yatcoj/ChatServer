4635d40867323c88ae87806a05d8404aa2e98369
John Yatco <yatcoj@wit.edu>
2017-11-08 11:42:36 -0500

    Like nothing

--- src/AskClient.java
+++ src/AskClient.java
@@ -11,11 +11,10 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 
-
 public class AskClient 
 {
 
-	private JFrame frame;
+	private JFrame frmMenu;
 
 	/**
 	 * Launch the application.
@@ -29,7 +28,7 @@ public class AskClient
 				try 
 				{
 					AskClient window = new AskClient();
-					window.frame.setVisible(true);
+					window.frmMenu.setVisible(true);
 				} 
 				catch (Exception e) 
 				{
@@ -52,10 +51,11 @@ public class AskClient
 	 */
 	private void initialize()
 	{
-		frame = new JFrame();
-		frame.setResizable(false);
-		frame.setBounds(100, 100, 181, 126);
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frmMenu = new JFrame();
+		frmMenu.setTitle("Menu");
+		frmMenu.setBounds(100, 100, 348, 126);
+		frmMenu.setLocationRelativeTo(null);
+		frmMenu.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
 		JButton btnNewButton = new JButton("Client");
 		btnNewButton.addActionListener(new ActionListener() 
@@ -64,7 +64,7 @@ public class AskClient
 			{
 				
 				MainWindow main = new MainWindow();
-				frame.setVisible(false);
+				frmMenu.setVisible(false);
 			}
 		});
 		
@@ -75,12 +75,12 @@ public class AskClient
 			{
 				int port = 1111;
 				Server serv = new Server(port);
-				frame.setVisible(false);
+				frmMenu.setVisible(false);
 			}
 		});
 		
 		JLabel lblLaunch = new JLabel("Launch");
-		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		GroupLayout groupLayout = new GroupLayout(frmMenu.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
@@ -88,25 +88,25 @@ public class AskClient
 						.addGroup(groupLayout.createSequentialGroup()
 							.addContainerGap()
 							.addComponent(btnNewButton)
-							.addPreferredGap(ComponentPlacement.RELATED, 76, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.RELATED, 170, Short.MAX_VALUE)
 							.addComponent(btnNewButton_1))
 						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(63)
+							.addGap(144)
 							.addComponent(lblLaunch)))
 					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-					.addContainerGap(35, Short.MAX_VALUE)
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
 					.addComponent(lblLaunch)
-					.addGap(18)
+					.addPreferredGap(ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(btnNewButton)
 						.addComponent(btnNewButton_1))
 					.addContainerGap())
 		);
-		frame.getContentPane().setLayout(groupLayout);
+		frmMenu.getContentPane().setLayout(groupLayout);
 	}
 
 }
--- src/Server.java
+++ src/Server.java
@@ -35,7 +35,6 @@ public class Server
 		}
 		catch (IOException e)
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
