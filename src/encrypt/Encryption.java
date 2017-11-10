b1eb5d7c6933158bb13a026d74cc6bb507e1525b
Brian Galvin <galvinl@wit.edu>
2017-11-10 11:51:00 -0500

    Encrypt still needs some work

--- src/encrypt/Encryption.java
+++ src/encrypt/Encryption.java
@@ -32,18 +32,48 @@ public class Encryption
 				if(message.toCharArray()[i] == allChar.toCharArray()[j])
 				{
 					encM += jumbChar.toCharArray()[j];
+					encM +=	allChar.toCharArray()[r.nextInt(allChar.length())];
 				}
 			}
 		}
-		
 		return encM;
 	}
 	
 	//Should be just as easy as calling .decrypt(text); to decrypt
 	public static String decrypt(String message)
 	{
+		String decMH = "";
 		String decM = "";
+		String allChar = "abcdefghijklmnopqrstuvwxyz1234567890`-=~_+!@#$%^&*()[]\\;',./{}|:\"<>? 	ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String jumbChar = "";
+
+		Random r = new Random(allChar.length());
+		//makes the encoded text
+		for(int i = 0; i < allChar.length(); i++)
+		{
+			int ri = r.nextInt(allChar.length());
+			jumbChar += allChar.toCharArray()[ri];
+		}
 		
+		//Removes the trash numbers
+		for(int i = 0; i < message.length(); i++)
+		{
+			if(i%2 == 0)
+			{
+				decMH += message.toCharArray()[i];
+			}
+		}
+		
+		for(int i = 0; i < decMH.length(); i++)
+		{
+			for(int j = 0; j < jumbChar.length(); j++)
+			{
+				if((""+decMH.toCharArray()[i]).equals( ""+jumbChar.toCharArray()[j]))
+				{
+					decM += allChar.toCharArray()[j];
+				}
+			}
+		}
 		
 		return decM;
 	}
@@ -53,6 +83,8 @@ public class Encryption
 	public static void main(String[] args) 
 	{
 		String b = encrypt("abcdefghijklmnopqrstuvwxyz");
+		String c = decrypt(b);
 		System.out.println(b);
+		System.out.println(c);
 	}
 }
