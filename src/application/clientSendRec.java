ee8aa82b38540c53eba50d0bcca85f357741aba4
John Yatco <yatcoj@wit.edu>
2017-11-28 22:15:34 -0500

    import stuff

--- src/application/clientSendRec.java
+++ src/application/clientSendRec.java
@@ -15,6 +15,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 
 public class clientSendRec
 {
